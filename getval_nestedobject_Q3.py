00da37ee6b9a19d9e7ab4f97f012fd1c1fc9b110
AmrutaJakhadi <amrutajakhadi@gmail.com>
2022-03-27 21:13:39 +0100

    Update getval_nestedobject_Q3.py

--- getval_nestedobject_Q3.py
+++ getval_nestedobject_Q3.py
@@ -1,14 +1,20 @@
-dict1 = {'x':{'y':{'z':'a'}}}  ##define dictionary as an object received
+dict1 = {'a':{'b':{'c':'d'}}}  ##define dictionary as an object received
+key1 = 'b'  ##key to be passed 
 
-def printvalue(dict1):
-    for k, v in dict1.items():
+dict2 = {'x':{'y':{'z':'a'}}}
+key2 = 'y'
+
+def printvalue(dict_current,key_current):
+    for k, v in dict_current.items():
         if isinstance(v, dict):  ##check if the v type is dictionary
-            printvalue(v)
+            printvalue(v,key_current)
         else:
             #print("{0} : {1}".format(k, v))  ##print key-value pair
-            print(v) ##print only value
+            print("Key is %s and Value is %s " %(key_current,v)) ##print  value
 
-printvalue(dict1)
+printvalue(dict1,key1)
+printvalue(dict2,key2)
+
             
             
             
